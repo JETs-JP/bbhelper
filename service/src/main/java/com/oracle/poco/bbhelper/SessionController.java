720c7a44ad986cb37757c1825c2e7de50ea6ceec
charlier-shoe <charlier.shoe@gmail.com>
2017-06-20 23:54:34 +0900

    ログイン成功／失敗時のログ出力を追加

--- service/src/main/java/com/oracle/poco/bbhelper/SessionController.java
+++ service/src/main/java/com/oracle/poco/bbhelper/SessionController.java
@@ -2,6 +2,10 @@ package com.oracle.poco.bbhelper;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.oracle.poco.bbhelper.log.BasicMessage;
+import com.oracle.poco.bbhelper.log.BbhelperLogger;
+import com.oracle.poco.bbhelper.log.Operation;
+import com.oracle.poco.bbhelper.log.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.http.HttpHeaders;
@@ -18,10 +22,11 @@ import com.oracle.poco.bbhelper.exception.BbhelperException;
 import jp.gr.java_conf.hhayakawa_jp.beehive4j.BeehiveContext;
 import jp.gr.java_conf.hhayakawa_jp.beehive4j.exception.BeehiveApiFaultException;
 
-// TODO このクラスにロギングを実装する
 @RestController
 @RequestMapping("/api/session")
 public class SessionController {
+
+    private static final BbhelperLogger logger = BbhelperLogger.getLogger(SessionController.class);
 
     @Autowired
     private ApplicationProperties properties;
@@ -33,24 +38,30 @@ public class SessionController {
     private AutowireCapableBeanFactory factory;
 
     @RequestMapping(path = "/actions/login",
-                    method = RequestMethod.GET)
+            method = RequestMethod.GET)
     public ResponseEntity<String> login(HttpServletRequest request) throws BbhelperException {
         final String basicAuthHeader = request.getHeader("Authorization");
         if (basicAuthHeader == null || basicAuthHeader.length() == 0) {
+            this.logger.info(new BasicMessage(Operation.LOGIN, Result.FAIL, "no credential."));
             throw new BbhelperBadRequestException();
         }
+        String session_id;
         try {
             BeehiveContext context =
                     BeehiveContext.getBeehiveContext(properties.getBeehiveUrl(), basicAuthHeader);
             Session session = new Session(context);
             factory.autowireBean(session);
-            String session_id = sessionPool.put(session);
-            HttpHeaders headers = new HttpHeaders();
-            headers.add(Constants.HEADER_KEY_BBH_AUTHORIZED_SESSION, session_id);
-            return new ResponseEntity<>(null, headers, HttpStatus.OK);
+            session_id = sessionPool.put(session);
+            this.logger.info(new BasicMessage(Operation.LOGIN, Result.SUCCESS, "login"));
         } catch (BeehiveApiFaultException e) {
-            throw new BbhelperBeehive4jException();
+            // TODO エラーの原因をログに残す
+            this.logger.info(new BasicMessage(Operation.LOGIN, Result.FAIL, "login failed."));
+            // TODO 失敗の原因に合わせた例外を上げる
+            throw new BbhelperBeehive4jException(e);
         }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(Constants.HEADER_KEY_BBH_AUTHORIZED_SESSION, session_id);
+        return new ResponseEntity<>(null, headers, HttpStatus.OK);
     }
 
 }
