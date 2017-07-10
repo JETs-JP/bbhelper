fd999710f0b32fc8ab1f9b7217e0e7a69ead1fb8
hhiroshell <hhiroshell@gmail.com>
2017-07-11 00:53:00 +0900

    beehive4jを並列実行したときのエラーレスポンスに個々のエラーのメッセージを含める

--- service/src/main/java/com/oracle/poco/bbhelper/ExceptionControllerAdvice.java
+++ service/src/main/java/com/oracle/poco/bbhelper/ExceptionControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.oracle.poco.bbhelper;
 
+import com.oracle.poco.bbhelper.exception.BbhelperBeehive4jException;
+import com.oracle.poco.bbhelper.exception.BbhelperBeehive4jParallelInvocationException;
 import com.oracle.poco.bbhelper.exception.BbhelperException;
 import com.oracle.poco.bbhelper.exception.BbhelperValidationFailureException;
 import com.oracle.poco.bbhelper.log.BbhelperLogger;
@@ -16,38 +18,66 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.List;
+
 @ControllerAdvice
 public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
 
     private static final BbhelperLogger logger =
             BbhelperLogger.getLogger(ExceptionControllerAdvice.class);
 
-    // 独自に定義した例外用のハンドラ
+    @ExceptionHandler
+    public ResponseEntity<Object> handleBbhelperBeehive4jParallelInvocationException(
+            BbhelperBeehive4jParallelInvocationException ex,
+            WebRequest request) {
+        List<BbhelperBeehive4jException> causes = ex.getCauses();
+        if (causes.size() == 1) {
+            return handleExceptionInternal(
+                    causes.get(0), null, null, causes.get(0).getStatus(), request);
+        }
+        ErrorResponse error = new ErrorResponse(
+                ex.getStatus().value(), ex.getStatus().getReasonPhrase(),
+                ex.getClass().getName(), ex.getLocalizedMessage());
+        causes.forEach(e -> error.addDetail(e.getLocalizedMessage()));
+        return super.handleExceptionInternal(ex, error, null, ex.getStatus(), request);
+    }
+
     @ExceptionHandler
     public ResponseEntity<Object> handleBbhelperException(BbhelperException ex, WebRequest request) {
         return handleExceptionInternal(ex, null, null, ex.getStatus(), request);
     }
 
     @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorResponse error = new ErrorResponse(status.value(), status.getReasonPhrase(),
+                ex.getClass().getName(), ex.getLocalizedMessage());
+        return super.handleExceptionInternal(ex, error, headers, status, request);
+    }
+
+    @Override
     protected ResponseEntity<Object> handleBindException(
             BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return handleValidationFalure(ex, ex.getBindingResult(), headers, status, request);
+        return handleValidationFailure(ex, ex.getBindingResult(), headers, status, request);
     }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status,
             WebRequest request) {
-        return handleValidationFalure(ex, ex.getBindingResult(), headers, status, request);
+        return handleValidationFailure(ex, ex.getBindingResult(), headers, status, request);
     }
 
-    private ResponseEntity<Object> handleValidationFalure(
+    private ResponseEntity<Object> handleValidationFailure(
             Throwable cause, BindingResult bindingResult, HttpHeaders headers, HttpStatus status,
             WebRequest request) {
         BbhelperValidationFailureException bbhe =
                 new BbhelperValidationFailureException(cause, bindingResult);
+        /*
+         * バリデーション時の例外の処理はこの例外ハンドラにに集約するので、ロギングもここで行う。
+         * 個別のControllerメソッドではバリデーションのロギングは記述しない。
+         */
         logger.info(new ErrorMessage(Operation.VALIDATION, bbhe));
-
         ErrorResponse error = new ErrorResponse(status.value(), status.getReasonPhrase(),
                 bbhe.getClass().getName(), bbhe.getLocalizedMessage());
         bindingResult.getGlobalErrors().stream().forEach(
@@ -55,18 +85,6 @@ public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
         bindingResult.getFieldErrors().stream().forEach(
                 e -> error.addDetail(e.getDefaultMessage()));
         return super.handleExceptionInternal(bbhe, error, headers, status, request);
-    }
-
-    /*
-     * すべての例外のハンドラから呼び出される処理。
-     * ここでエラーレスポンスを作成／適用することで、すべてのException型に対するエラーレスポンスに反映される。
-     */
-    @Override
-    protected ResponseEntity<Object> handleExceptionInternal(
-            Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        ErrorResponse error = new ErrorResponse(status.value(), status.getReasonPhrase(),
-                ex.getClass().getName(), ex.getLocalizedMessage());
-        return super.handleExceptionInternal(ex, error, headers, status, request);
     }
 
 }
