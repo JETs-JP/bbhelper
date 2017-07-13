72b506e924c4805389266151f1f0f9fc4c7026f9
hhiroshell <hhiroshell@gmail.com>
2017-07-13 14:26:49 +0900

    明示的にハンドリングしていない例外が起きたときに、通常と形式が異なるレスポンスが返却される問題を修正

--- service/src/main/java/com/oracle/poco/bbhelper/ExceptionControllerAdvice.java
+++ service/src/main/java/com/oracle/poco/bbhelper/ExceptionControllerAdvice.java
@@ -19,20 +19,44 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
 @ControllerAdvice
 public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
 
     private static final BbhelperLogger logger =
             BbhelperLogger.getLogger(ExceptionControllerAdvice.class);
 
+    /**
+     * このクラスで明示的にハンドリングしていない例外を処理する
+     * 明示的にハンドリングしていない例外は、システムエラーとして固定のレスポンスを返却する
+     *
+     * @param ex        発生した例外
+     * @param request   リクエスト
+     * @return          レスポンス
+     */
+    @ExceptionHandler
+    public ResponseEntity<Object> handleSystemException(Exception ex, WebRequest request) {
+        ErrorResponse error = new ErrorResponse(
+                INTERNAL_SERVER_ERROR.value(), INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                "System Error", "System error is occurred.");
+        return super.handleExceptionInternal(ex, error, null, null, request);
+    }
+
+    /**
+     * Beehiveサーバーへの複数回のアクセスを並列実行したときの例外をハンドリングする
+     *
+     * @param ex        発生した例外を束ねる例外クラス
+     * @param request   リクエスト
+     * @return          レスポンス
+     */
     @ExceptionHandler
     public ResponseEntity<Object> handleBbhelperBeehive4jParallelInvocationException(
             BbhelperBeehive4jParallelInvocationException ex,
             WebRequest request) {
         List<BbhelperException> causes = ex.getCauses();
         if (causes.size() == 1) {
-            return handleExceptionInternal(
-                    causes.get(0), null, null, causes.get(0).getStatus(), request);
+            return handleBbhelperException(causes.get(0), request);
         }
         ErrorResponse error = new ErrorResponse(
                 ex.getStatus().value(), ex.getStatus().getReasonPhrase(),
@@ -41,25 +65,33 @@ public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
         return super.handleExceptionInternal(ex, error, null, ex.getStatus(), request);
     }
 
+    /**
+     * このアプリケーションで定義しているカスタム例外をハンドリングする
+     *
+     * @param ex        発生した例外
+     * @param request   リクエスト
+     * @return          レスポンス
+     */
     @ExceptionHandler
     public ResponseEntity<Object> handleBbhelperException(BbhelperException ex, WebRequest request) {
-        return handleExceptionInternal(ex, null, null, ex.getStatus(), request);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleExceptionInternal(
-            Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        ErrorResponse error = new ErrorResponse(status.value(), status.getReasonPhrase(),
+        ErrorResponse error = new ErrorResponse(
+                ex.getStatus().value(), ex.getStatus().getReasonPhrase(),
                 ex.getClass().getName(), ex.getLocalizedMessage());
-        return super.handleExceptionInternal(ex, error, headers, status, request);
+        return super.handleExceptionInternal(ex, error, null, ex.getStatus(), request);
     }
 
+    /*
+     * 入力チェック時の例外をハンドリングするためにこのメソッドをオーバーライドする
+     */
     @Override
     protected ResponseEntity<Object> handleBindException(
             BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return handleValidationFailure(ex, ex.getBindingResult(), headers, status, request);
     }
 
+    /*
+     * 入力チェック時の例外をハンドリングするためにこのメソッドをオーバーライドする
+     */
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status,
