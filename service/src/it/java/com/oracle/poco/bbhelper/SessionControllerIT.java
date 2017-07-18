8d8471eba9c39be911abd528cdd569d35070babc
hhiroshell <hhiroshell@gmail.com>
2017-07-18 15:04:23 +0900

    SessionControllerに対する結合テストを追加

---
+++ service/src/it/java/com/oracle/poco/bbhelper/SessionControllerIT.java
@@ -0,0 +1,115 @@
+package com.oracle.poco.bbhelper;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Base64;
+
+import static com.oracle.poco.bbhelper.Constants.HEADER_KEY_BBH_AUTHORIZED_SESSION;
+import static com.oracle.poco.bbhelper.TestConstants.PROPERTY_KEY_TEST_BEEHIVE_PASSWORD;
+import static com.oracle.poco.bbhelper.TestConstants.PROPERTY_KEY_TEST_BEEHIVE_USERNAME;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+        classes = {Application.class},
+        initializers = {ConfigFileApplicationContextInitializer.class})
+@WebAppConfiguration
+public class SessionControllerIT {
+
+    @Autowired
+    WebApplicationContext context;
+
+    MockMvc mockMvc;
+
+    String username, password;
+
+    @Before
+    public void setupMockMvc() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Before
+    public void prepareCredentials() {
+        this.username = System.getProperty(PROPERTY_KEY_TEST_BEEHIVE_USERNAME);
+        this.password = System.getProperty(PROPERTY_KEY_TEST_BEEHIVE_PASSWORD);
+    }
+
+    private static final String RESPONSE_NO_CREDENTIALS =
+            "{" +
+            "   \"status\": 401," +
+            "   \"error\": \"Unauthorized\"," +
+            "   \"code\": \"com.oracle.poco.bbhelper.exception.BbhelperNoCredentialsException\"," +
+            "   \"message\": \"No credentials.\"" +
+            "}";
+
+
+    private static final String RESPONSE_INVALID_CREDENTIALS =
+            "{" +
+            "   \"status\": 401," +
+            "   \"error\": \"Unauthorized\"," +
+            "   \"code\": \"com.oracle.poco.bbhelper.exception.BbhelperInvalidCredentialsException\"," +
+            "   \"message\": \"Incorrect username or password.\"" +
+            "}";
+
+    @Test
+    public void NoCredentials() throws Exception {
+        mockMvc.perform(get("/api/session/actions/login"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().json(RESPONSE_NO_CREDENTIALS, true));
+    }
+
+    @Test
+    public void InvalidUsername() throws Exception {
+        mockMvc.perform(get("/api/session/actions/login")
+                .header(HttpHeaders.AUTHORIZATION,
+                        makeBasicAuthString(username + "_", password)))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().json(RESPONSE_INVALID_CREDENTIALS, true));
+    }
+
+    @Test
+    public void InvalidPassword() throws Exception {
+        mockMvc.perform(get("/api/session/actions/login")
+                .header(HttpHeaders.AUTHORIZATION,
+                        makeBasicAuthString(username,password + "_")))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().json(RESPONSE_INVALID_CREDENTIALS, true));
+    }
+
+    @Test
+    public void Success() throws Exception {
+        mockMvc.perform(get("/api/session/actions/login")
+                .header(HttpHeaders.AUTHORIZATION, makeBasicAuthString(username, password)))
+                .andExpect(status().isOk())
+                .andExpect(header().string(
+                        HEADER_KEY_BBH_AUTHORIZED_SESSION, not(isEmptyOrNullString())));
+    }
+
+    private static String makeBasicAuthString(String user, String password) {
+        if (user.contains(":")) {
+            throw new IllegalArgumentException(
+                    "User name must not contain \":\".");
+        }
+        String src = user.trim() + ":" + password;
+        byte[] encoded = Base64.getEncoder().encode(src.getBytes());
+        return "Basic " + new String(encoded);
+    }
+
+}
---
+++ service/src/it/java/com/oracle/poco/bbhelper/TestConstants.java
@@ -0,0 +1,9 @@
+package com.oracle.poco.bbhelper;
+
+public class TestConstants {
+
+    public static final String PROPERTY_KEY_TEST_BEEHIVE_USERNAME = "test.beehive.username";
+
+    public static final String PROPERTY_KEY_TEST_BEEHIVE_PASSWORD = "test.beehive.password";
+
+}
